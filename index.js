ab9def5ebc2dccf58b512d0f79477e6ccf5dc489
Sakib Mia <contact.sakibmia99@gmail.com>
2025-08-28 10:00:01 +0600

    add click btn ++

---
+++ index.js
@@ -0,0 +1,93 @@
+// heard click ++ section 
+let heardCount = 0;
+ const heardDisplay = document.getElementById("heardCount");
+document.getElementById("heard-btn1").addEventListener('click', function() {
+    heardCount++;
+    heardDisplay.textContent = heardCount;
+  });  
+
+  document.getElementById("heard-btn2").addEventListener('click', function() {
+    heardCount++;
+    heardDisplay.textContent = heardCount;
+  });  
+    document.getElementById("heard-btn3").addEventListener('click', function() {
+    heardCount++;
+    heardDisplay.textContent = heardCount;
+  });  
+    document.getElementById("heard-btn4").addEventListener('click', function() {
+    heardCount++;
+    heardDisplay.textContent = heardCount;
+  });  
+    document.getElementById("heard-btn5").addEventListener('click', function() {
+    heardCount++;
+    heardDisplay.textContent = heardCount;
+  });  
+    document.getElementById("heard-btn6").addEventListener('click', function() {
+    heardCount++;
+    heardDisplay.textContent = heardCount;
+  });  
+    document.getElementById("heard-btn7").addEventListener('click', function() {
+    heardCount++;
+    heardDisplay.textContent = heardCount;
+  });  
+    document.getElementById("heard-btn8").addEventListener('click', function() {
+    heardCount++;
+    heardDisplay.textContent = heardCount;
+  });  
+    document.getElementById("heard-btn9").addEventListener('click', function() {
+    heardCount++;
+    heardDisplay.textContent = heardCount;
+  });  
+
+//   copy click ++ section
+
+let copyCount = 0;
+ const copyDisplay = document.getElementById("copyCount");
+ document.getElementById("copy-btn1").addEventListener('click', function() {
+    copyCount++;
+    copyDisplay.textContent = copyCount;
+  });  
+  document.getElementById("copy-btn2").addEventListener('click', function() {
+    copyCount++;
+    copyDisplay.textContent = copyCount;
+  });  
+
+  document.getElementById("copy-btn3").addEventListener('click', function() {
+    copyCount++;
+    copyDisplay.textContent = copyCount;
+  });  
+
+  document.getElementById("copy-btn4").addEventListener('click', function() {
+    copyCount++;
+    copyDisplay.textContent = copyCount;
+  });  
+
+  document.getElementById("copy-btn5").addEventListener('click', function() {
+    copyCount++;
+    copyDisplay.textContent = copyCount;
+  });  
+
+  document.getElementById("copy-btn6").addEventListener('click', function() {
+    copyCount++;
+    copyDisplay.textContent = copyCount;
+  });  
+
+  document.getElementById("copy-btn7").addEventListener('click', function() {
+    copyCount++;
+    copyDisplay.textContent = copyCount;
+  });  
+
+  document.getElementById("copy-btn8").addEventListener('click', function() {
+    copyCount++;
+    copyDisplay.textContent = copyCount;
+  });  
+
+  document.getElementById("copy-btn9").addEventListener('click', function() {
+    copyCount++;
+    copyDisplay.textContent = copyCount;
+  });  
+
+  
+
+
+
